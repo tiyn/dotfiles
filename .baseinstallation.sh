0e87ae9796c28242adb9fd7e465735565878b439
tiynger <kantemarten@googlemail.com>
2019-07-02 15:29:41 +0200

    adding .baseinstallation.sh

---
+++ .baseinstallation.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+##remove firefox
+pacman -Rns firefox
+
+##remove vifm
+pacman -Rns vifm
+
+##remove tree
+pacman -Rns tree
+
+##remove calcurse
+pacman -Rns calcurse
+
+##remove vi
+pacman -Rns vi
+
+
+##add ranger
+pacman -S ranger
+
+##add lsd
+pacman -S lsd
+
+##add discord
+git clone https://aur.archlinux.org/discord.git $HOME/downloads/set-ups/discord
+cd $HOME/downloads/set-ups/discord && makepkg -sirc
+
+##add lastpass-cli-git
+git clone https://aur.archlinux.org/lastpass-cli-git.git $HOME/downloads/set-ups/lastpass-cli-git
+cd $HOME/downloads/set-ups/lastpass-cli-git && makepkg -sirc
+
+##add maxima
+pacman -S maxima
+
+##add mutt-wizard-git
+git clone https://aur.archlinux.org/mutt-wizard-git.git $HOME/downloads/set-ups/mutt-wizard-git
+cd $HOME/downloads/set-ups/mutt-wizard-git && makepkg -sirc
+
+##add spotify
+git clone https://aur.archlinux.org/spotify.git $HOME/downloads/set-ups/spotify
+cd $HOME/downloads/set-ups/spotfy && makepkg -sirc
+
+##add tor-browser
+git clone https://aur.archlinux.org/tor-browser.git $HOME/downloads/set-ups/tor-browser
+cd $HOME/downloads/set-ups/tor-browser && makepkg -sirc
+
+##add vivaldi
+git clone https://aur.archlinux.org/vivaldi.git $HOME/downloads/set-ups/vivaldi
+cd $HOME/downloads/set-ups/vivaldi && makepkg -sirc
+
+##add jdk8-openjdk
+pacman -S jdk8-openjdk
+
+##add zip
+pacman -S jdk8-openjdk
+
+##add intellij-idea-community-edition
+pacman -S intellij-idea-community-edition
+
+##add rtv
+pacman -S rtv
+
+##add owncloud-client
+pacman -S owncloud-client
+
+##add qt5ct
+pacman -S qt5ct
+
+##add nm-connection-editor
+pacman -S nm-connection-editor
+
+##add cmatrix
+pacman -S cmatrix
+
+##add imagemagick
+pacman -S imagemagick
+
+##add texlive
+pacman -S texlive-most
+pacman -S texlive-lang
+
+##add xournalpp
+pacman -S xournalpp
+
+##add code
+git clone https://aur.archlinux.org/visual-studio-code-bin.git $HOME/downloads/set-ups/visual-studio-code-bin
+cd $HOME/downloads/set-ups/visual-studio-code-bin && makepkg -sirc
