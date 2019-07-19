5b014d60fa19588f31ce4c0289ee2c78f72a52e5
tiynger <kantemarten@googlemail.com>
2019-07-19 21:28:52 +0200

    restructuring

--- .dotinstall/baseinstallation.sh
+++ .dotinstall/baseinstallation.sh
@@ -1,34 +1,57 @@
 #!/bin/bash
 
-## remove vifm
-sudo pacman -Rns vifm
+## remove calcurse
+sudo pacman -Rns calcurse
 
 ## remove tree
 sudo pacman -Rns tree
 
-## remove calcurse
-sudo pacman -Rns calcurse
-
 ## remove vi
 sudo pacman -Rns vi
 
+## remove vifm
+sudo pacman -Rns vifm
 
-## add neofetch
-sudo pacman -S neofetch
 
-## add ranger
-sudo pacman -S ranger
+## add acpi
+sudo pacman -S acpi
 
-## add lsd
-sudo pacman -S lsd
+## add cmatrix
+sudo pacman -S cmatrix
+
+## add code
+git clone https://aur.archlinux.org/visual-studio-code-bin.git $HOME/downloads/set-ups/visual-studio-code-bin
+cd $HOME/downloads/set-ups/visual-studio-code-bin && makepkg -sirc
 
 ## add discord
 git clone https://aur.archlinux.org/discord.git $HOME/downloads/set-ups/discord
 cd $HOME/downloads/set-ups/discord && makepkg -sirc
 
+## add docker
+sudo pacman -S docker
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+
+## add imagemagick
+sudo pacman -S imagemagick
+
+## add intellij-idea-community-edition
+sudo pacman -S intellij-idea-community-edition
+
+## add jdk8-openjdk
+sudo pacman -S jdk8-openjdk
+
 ## add lastpass-cli-git
 git clone https://aur.archlinux.org/lastpass-cli-git.git $HOME/downloads/set-ups/lastpass-cli-git
 cd $HOME/downloads/set-ups/lastpass-cli-git && makepkg -sirc
+
+## add lsd
+sudo pacman -S lsd
+
+## add lynx
+sudo pacman -S lynx
 
 ## add maxima
 sudo pacman -S maxima
@@ -37,9 +60,40 @@ sudo pacman -S maxima
 git clone https://aur.archlinux.org/mutt-wizard-git.git $HOME/downloads/set-ups/mutt-wizard-git
 cd $HOME/downloads/set-ups/mutt-wizard-git && makepkg -sirc
 
+## add neofetch
+sudo pacman -S neofetch
+
+## add nm-connection-editor
+sudo pacman -S nm-connection-editor
+
+## add openssh
+sudo pacman -S openssh
+
+## add owncloud-client
+sudo pacman -S owncloud-client
+
+## add qt5ct
+sudo pacman -S qt5ct
+
+## add ranger
+sudo pacman -S ranger
+
+## add rsync
+sudo pacman -S rsync
+
+## add rtv
+sudo pacman -S rtv
+
 ## add spotify
 git clone https://aur.archlinux.org/spotify.git $HOME/downloads/set-ups/spotify
 cd $HOME/downloads/set-ups/spotfy && makepkg -sirc
+
+## add sshpass
+sudo pacman -S sshpass
+
+## add texlive
+sudo pacman -S texlive-most
+sudo pacman -S texlive-lang
 
 ## add tor-browser
 git clone https://aur.archlinux.org/tor-browser.git $HOME/downloads/set-ups/tor-browser
@@ -49,43 +103,9 @@ cd $HOME/downloads/set-ups/tor-browser && makepkg -sirc
 git clone https://aur.archlinux.org/vivaldi.git $HOME/downloads/set-ups/vivaldi
 cd $HOME/downloads/set-ups/vivaldi && makepkg -sirc
 
-## add jdk8-openjdk
-sudo pacman -S jdk8-openjdk
+## add xournalpp
+sudo pacman -S xournalpp
 
 ## add zip
 sudo pacman -S jdk8-openjdk
 
-## add intellij-idea-community-edition
-sudo pacman -S intellij-idea-community-edition
-
-## add rtv
-sudo pacman -S rtv
-
-## add owncloud-client
-sudo pacman -S owncloud-client
-
-## add qt5ct
-sudo pacman -S qt5ct
-
-## add nm-connection-editor
-sudo pacman -S nm-connection-editor
-
-## add cmatrix
-sudo pacman -S cmatrix
-
-## add imagemagick
-sudo pacman -S imagemagick
-
-## add texlive
-sudo pacman -S texlive-most
-sudo pacman -S texlive-lang
-
-## add xournalpp
-sudo pacman -S xournalpp
-
-## add code
-git clone https://aur.archlinux.org/visual-studio-code-bin.git $HOME/downloads/set-ups/visual-studio-code-bin
-cd $HOME/downloads/set-ups/visual-studio-code-bin && makepkg -sirc
-
-## add lynx
-sudo pacman -S lynx
