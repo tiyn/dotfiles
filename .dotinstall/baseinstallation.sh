366c9394b3251a7ebb0396753c9c00a8c1005b48
tiynger <kantemarten@googlemail.com>
2019-07-20 20:20:24 +0200

    update for adding sc-im

--- .dotinstall/baseinstallation.sh
+++ .dotinstall/baseinstallation.sh
@@ -20,12 +20,12 @@ sudo pacman -S acpi
 sudo pacman -S cmatrix
 
 ## add code
-git clone https://aur.archlinux.org/visual-studio-code-bin.git $HOME/downloads/set-ups/visual-studio-code-bin
-cd $HOME/downloads/set-ups/visual-studio-code-bin && makepkg -sirc
+git clone https://aur.archlinux.org/visual-studio-code-bin.git $HOME/downloads/set\-ups/visual-studio-code-bin
+cd $HOME/downloads/set\-ups/visual-studio-code-bin && makepkg -sirc
 
 ## add discord
-git clone https://aur.archlinux.org/discord.git $HOME/downloads/set-ups/discord
-cd $HOME/downloads/set-ups/discord && makepkg -sirc
+git clone https://aur.archlinux.org/discord.git $HOME/downloads/set\-ups/discord
+cd $HOME/downloads/set\-ups/discord && makepkg -sirc
 
 ## add docker
 sudo pacman -S docker
@@ -44,8 +44,8 @@ sudo pacman -S intellij-idea-community-edition
 sudo pacman -S jdk8-openjdk
 
 ## add lastpass-cli-git
-git clone https://aur.archlinux.org/lastpass-cli-git.git $HOME/downloads/set-ups/lastpass-cli-git
-cd $HOME/downloads/set-ups/lastpass-cli-git && makepkg -sirc
+git clone https://aur.archlinux.org/lastpass-cli-git.git $HOME/downloads/set\-ups/lastpass-cli-git
+cd $HOME/downloads/set\-ups/lastpass-cli-git && makepkg -sirc
 
 ## add lsd
 sudo pacman -S lsd
@@ -57,8 +57,8 @@ sudo pacman -S lynx
 sudo pacman -S maxima
 
 ## add mutt-wizard-git
-git clone https://aur.archlinux.org/mutt-wizard-git.git $HOME/downloads/set-ups/mutt-wizard-git
-cd $HOME/downloads/set-ups/mutt-wizard-git && makepkg -sirc
+git clone https://aur.archlinux.org/mutt-wizard-git.git $HOME/downloads/set\-ups/mutt-wizard-git
+cd $HOME/downloads/set\-ups/mutt-wizard-git && makepkg -sirc
 
 ## add neofetch
 sudo pacman -S neofetch
@@ -84,9 +84,13 @@ sudo pacman -S rsync
 ## add rtv
 sudo pacman -S rtv
 
+## add scim
+git clone https://aur.archlinux.org/sc-im.git $HOME/downloads/set\-ups/sc-im
+cd $HOME/downloads/set\-ups/sc-im && makepkg -sirc
+
 ## add spotify
-git clone https://aur.archlinux.org/spotify.git $HOME/downloads/set-ups/spotify
-cd $HOME/downloads/set-ups/spotfy && makepkg -sirc
+git clone https://aur.archlinux.org/spotify.git $HOME/downloads/set\-ups/spotify
+cd $HOME/downloads/set\-ups/spotfy && makepkg -sirc
 
 ## add sshpass
 sudo pacman -S sshpass
@@ -96,12 +100,12 @@ sudo pacman -S texlive-most
 sudo pacman -S texlive-lang
 
 ## add tor-browser
-git clone https://aur.archlinux.org/tor-browser.git $HOME/downloads/set-ups/tor-browser
-cd $HOME/downloads/set-ups/tor-browser && makepkg -sirc
+git clone https://aur.archlinux.org/tor-browser.git $HOME/downloads/set\-ups/tor-browser
+cd $HOME/downloads/set\-ups/tor-browser && makepkg -sirc
 
 ## add vivaldi
-git clone https://aur.archlinux.org/vivaldi.git $HOME/downloads/set-ups/vivaldi
-cd $HOME/downloads/set-ups/vivaldi && makepkg -sirc
+git clone https://aur.archlinux.org/vivaldi.git $HOME/downloads/set\-ups/vivaldi
+cd $HOME/downloads/set\-ups/vivaldi && makepkg -sirc
 
 ## add xournalpp
 sudo pacman -S xournalpp
