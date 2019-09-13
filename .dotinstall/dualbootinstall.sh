f53c29bd5140cd40ae0991c87f696749cb459e70
TiynGER <43725244+TiynGER@users.noreply.github.com>
2019-09-13 10:37:56 +0200

    1.0.0 cleanup and adding license

--- .dotinstall/dualbootinstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## installing os-prober
-sudo pacman os-prober
-
-## finding other os
-sudo grub-mkconfig -o /boot/grub/grub.cfg
--- .dotinstall/thinkpadinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-##adding thinkfan
-git clone https://aur.archlinux.org/thinkfan $HOME/downloads/set\-ups/thinkfan
-cd $HOME/downloads/set\-ups/thinkfan
-makepkg -sirc
-
-##adding support for touchpad
-pacman -S xf86-input-synaptics
