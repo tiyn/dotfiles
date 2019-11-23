be117a470e961f1dfc45ecb5166eb255781689c6
TiynGER <kantemarten@googlemail.com>
2019-11-23 12:08:12 +0100

    bugfix in thinkpad script

--- .config/#additionalinstall/thinkpadspecific.sh
+++ .config/#additionalinstall/thinkpadspecific.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo yay -S thinkfan lm_sensors
+yay -S thinkfan lm_sensors
 sudo modprobe thinkpad_acpi
 sudo cp /usr/share/doc/thinkfan/examples/thinkfan.conf.simple /etc/thinkfan.conf
 systemctl enable thinkfan
