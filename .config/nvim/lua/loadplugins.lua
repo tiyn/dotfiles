5996f8d110d696a1e54f26a167248e5cb8aa95d9
tiyn <tiyn@mail-mk.eu>
2023-08-25 04:58:36 +0200

    nvim: swapped colorizer plugin

--- .config/nvim/lua/loadplugins.lua
+++ .config/nvim/lua/loadplugins.lua
@@ -323,10 +323,29 @@ return require("lazy").setup({
 
     -- showing color of hex values, etc
     {
-      'norcalli/nvim-colorizer.lua',
+      'nvchad/nvim-colorizer.lua',
       opts = {
-        '*',
-        '!markdown'
+        filetypes = {
+          '*',
+          '!markdown'
+        },
+        user_default_options = {
+          RGB = true,
+          RRGGBB = true,
+          RRGGBBAA = true,
+          AARRGGBB = true,
+          rgb_fn = true,
+          hsl_fn = true,
+          css_fn = true,
+          mode = "virtualtext",
+          virtualtext = " ",
+        },
+        -- all the sub-options of filetypes apply to buftypes
+        buftypes = {
+          "*",
+          "!prompt",
+          "!popup"
+        },
       }
     },
 
