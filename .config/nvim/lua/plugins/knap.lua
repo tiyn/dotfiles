50f5b2e2c7abd88451db63e8d3d0fc99149f4561
tiyn <tiyn@mail-mk.eu>
2023-12-09 01:22:48 +0100

    nvim/vifm: improved handling of pdf files with semicolons and other special characters

--- .config/nvim/lua/plugins/knap.lua
+++ .config/nvim/lua/plugins/knap.lua
@@ -9,13 +9,13 @@ return {
         texoutputext = "pdf",
         textopdf = "pdflatex -synctex=1 -halt-on-error -interaction=batchmode %docroot%",
         textopdfviewerlaunch =
-        "zathura --synctex-editor-command 'nvim --headless -es --cmd \"lua require('\"'\"'knaphelper'\"'\"').relayjump('\"'\"'%servername%'\"'\"','\"'\"'%{input}'\"'\"',%{line},0)\"' %outputfile%",
+        "zathura --synctex-editor-command 'nvim --headless -es --cmd \"lua require('\"'\"'knaphelper'\"'\"').relayjump('\"'\"'%servername%'\"'\"','\"'\"'%{input}'\"'\"',%{line},0)\"' ./%outputfile%",
         textopdfviewerrefresh = "none",
         textopdfforwardjump = "zathura --synctex-forward=%line%:%column%:%srcfile% %outputfile%",
         -- markdown
-        mdoutputext = "html",
+        mdoutputext = "pdf",
         mdtopdf = "pandoc %docroot% -o %outputfile%",
-        mdtopdfviewerlaunch = "zathura %outputfile%",
+        mdtopdfviewerlaunch = "zathura ./%outputfile%",
         mdtohtml = "pandoc --standalone %docroot% -o %outputfile%",
         mdtohtmlviewerlaunch = "firefox --kiosk --new-window %outputfile%",
         mdtohtmlviewerrefresh = "none",
