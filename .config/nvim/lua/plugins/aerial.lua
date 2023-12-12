dca524be17ae26b5b6cba646cda1967d640c2b07
tiyn <tiyn@mail-mk.eu>
2023-12-12 03:41:10 +0100

    nvim: set aerial size

--- .config/nvim/lua/plugins/aerial.lua
+++ .config/nvim/lua/plugins/aerial.lua
@@ -2,7 +2,13 @@ return {
   -- ctags like menu
   {
     'stevearc/aerial.nvim',
-    opts = {},
+    opts = {
+      layout = {
+        max_width = { 50, 0.2 },
+        min_width = 25,
+        resize_to_content = false,
+      },
+    },
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "nvim-tree/nvim-web-devicons"
