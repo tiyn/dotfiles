23b2e6cd6f441e09ae0629d413b8c9bf1a967635
tiyn <tiyn@mail-mk.eu>
2023-12-12 02:15:03 +0100

    nvim: added topbar and same word marking

---
+++ .config/nvim/lua/plugins/barbecue.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    opts = {
+      theme = {
+        -- this highlight is used to override other highlights
+        -- you can take advantage of its `bg` and set a background throughout your winbar
+        -- (e.g. basename will look like this: { fg = "#c0caf5", bold = true })
+        normal = { bg = "#272727" },
+      }
+    },
+  }
+}
