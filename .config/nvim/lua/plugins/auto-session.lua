ace5eb5baac543709276bfe792ab8eb0d4657da8
tiyn <tiyn@posteo.eu>
2026-03-18 03:55:01 +0100

    NVIM: Added session management

---
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -0,0 +1,13 @@
+return {
+  -- session management with git support
+  "rmagatti/auto-session",
+  lazy = false,
+
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    suppressed_dirs = { "~/", "~/code", "~/downloads", "/" },
+    -- log_level = 'debug',
+  },
+}
