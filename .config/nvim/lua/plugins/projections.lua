63aa8f2db543d6c576b8a4237a9dbe207ae9ca58
tiyn <tiyn@posteo.eu>
2026-04-11 09:50:11 +0200

    Nvim: Moved autocmds into plugins

--- .config/nvim/lua/plugins/projections.lua
+++ .config/nvim/lua/plugins/projections.lua
@@ -29,7 +29,31 @@ return {
     },
   },
   config = function(_, opts)
-    require("projections").setup(opts)
+    local projections = require("projections")
+    local session = require("projections.session")
+
+    projections.setup(opts)
     require("telescope").load_extension("projections")
+
+    vim.api.nvim_create_autocmd("VimLeavePre", {
+      callback = function()
+        session.store(vim.loop.cwd())
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function()
+        if vim.fn.argc() ~= 0 then
+          return
+        end
+        vim.schedule(function()
+          local session_info = session.info(vim.loop.cwd())
+          if session_info ~= nil then
+            session.restore(vim.loop.cwd())
+          end
+        end)
+      end,
+      desc = "Restore last session automatically",
+    })
   end,
 }
